5be5d46d5a26c44c7ae51f577443f6e094309ce4
alexbrighten <alexjob3606@gmail.com>
2024-12-04 01:48:02 +0530

    Updated on the sql database

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,19 +1,24 @@
-import { Header } from "@/app/(marketing)/header";
+import { Header } from "./header";
 import { FeedWrapper } from "@/components/feed-wrapper";
 import { StickyWrapper } from "@/components/sticky-wrapper";
+import { UserProgress } from "@/components/user-progress";
 
 const LearnPage = () => {
     return (
         <div className="flex flex-row-reverse gap-[48px] px-6">
             <StickyWrapper>
-                My sticky content
+                <UserProgress
+                    activeCourse={{ title: "Course Title", imgSrc: "/box.png" }}
+                    hearts={5}
+                    points={100}
+                    hasActiveSubscription={false}
+                />
             </StickyWrapper>
             <FeedWrapper>
                 <Header title="My Page Title" />
-                My main content
             </FeedWrapper>
         </div>
     );
-}
+};
 
 export default LearnPage;
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -8,9 +8,9 @@ export default function Home() {
   return (
     <div className="max-w-[988px] mx-auto flex-1 w-full flex flex-col lg:flex-row items-center justify-center p-4 gap-2">
       {/* Image Section */}
-      <div className="relative w-[240px] h-[240px] lg:w-[424px] lg:h-[424px] mb-8 lg:m-0">
+      {/* <div className="relative w-[240px] h-[240px] lg:w-[424px] lg:h-[424px] mb-8 lg:m-0">
         <Image src="/next.svg" alt="Hero" width={424} height={424} />
-      </div>
+      </div> */}
 
       {/* Header Section */}
       <div className="flex flex-col items-center gap-y-8">
